47c32865a5a18bc3b9dd702ace870e0162ac1c9c
HarshvardhanRepaswal <me.harshvardhan2004@kgpian.iitkgp.ac.in>
2025-03-09 18:59:34 +0530

    Initialized Vite+React with TailwindCSS

---
+++ ssb/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-orange-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, commodi. Fuga tempora vitae qui similique odit temporibus officiis veritatis adipisci perspiciatis laudantium possimus perferendis natus, molestiae quibusdam quaerat ducimus, consequatur fugit nulla porro, assumenda quam illum tenetur. Sapiente ad, temporibus eveniet inventore quo expedita eum maiores molestiae necessitatibus. Rerum, esse.
+      </p>
+    </>
+  )
+}
+
+export default App
